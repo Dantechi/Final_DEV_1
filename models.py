66461be7f7017c8f2405600026de1f61861fcf6e
Sala 3 <sala3@FI-LabSistemas.ucatolica.loc>
2025-12-05 08:01:49 -0500

    variables del modelo juagdor

--- models.py
+++ models.py
@@ -1,5 +1,18 @@
 class Jugador():
-    pass
+    nombre: str
+    numero_camiseta: int
+    fecha_nacimiento: str
+    nacionalidad: str
+    altura_cm: int
+    peso_kg: int
+    pie_dominante:str  #izq o derecho
+    posicion: str #posicion o rol. delantero y todo eso
+    valor_jugador: int # aqui no se si es el valor monetario
+    año_club: int
+    estado_activo: bool # si esta activo es por que sigue siendo parte del club
+    estado_suspendido: bool # si recibe amonestacion esta en estado suspendido
+
+
 
 
 class Estadistica():
